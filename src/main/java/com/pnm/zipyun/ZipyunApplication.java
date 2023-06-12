f890aa8718b2eb0c593a4d5b571e83fe12dc7e70
bdh <bdhn1100@gmail.com>
2023-06-12 21:01:35 +0900

    git ignore 추가

---
+++ src/main/java/com/pnm/zipyun/ZipyunApplication.java
@@ -0,0 +1,13 @@
+package com.pnm.zipyun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipyunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipyunApplication.class, args);
+	}
+
+}
